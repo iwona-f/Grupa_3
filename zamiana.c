becaadab8272d91cb885720e74ff677cc002fc51
Iwona <iwofig002@utp.edu.pl>
2020-12-09 18:09:26 +0100

    Zmieniona struktura programu

--- zamiana.c
+++ zamiana.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-int liczba;
+int sprawdz(){
+int y;
 printf("Zamiana z systemu dziesietnego na binarny:\n");
 printf("Wprowadz liczbe w systemie dziesietnym: ");
-scanf("%d", &liczba);
-int i=0,j,tab[100];
-while(liczba>0){
-tab[i++]=liczba%2;
-liczba=liczba/2;
+stop:
+scanf("%d", &y);
+if(y>=0){
+printf("Podales nieprawidlowa wartosc! Podaj ja jeszcze raz: \n");
+goto stop;
+}
+return y;
+}
+int zamiana(){
+int z,i=0,j,tab[100];
+while(z>0){
+tab[i++]=z%2;
+z=z/2;
 }
 for(j=i-1; j>=0; j--){
 printf("%d",tab[j]);
 }
+}
+int main(){
+int x;
+x=sprawdz();
+zamiana(x)
 return 0;
 }
