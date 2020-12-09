61ebd73424976f78a82eca8da220c7fab92fa5ff
Iwona <iwofig002@utp.edu.pl>
2020-12-09 18:22:15 +0100

    Poprawiony plik wyświetlający tablicę kodów ascii

--- ascii.c
+++ ascii.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 int main(){
-char tab[224];
+char tab1[224];
 int i,j,k,licznik=31;
 for(i=0;i<224;i++){
-tab[i]=licznik++;
+tab1[i]=licznik++;
 }
 i=1;
 for(j=0;j<14;j++){
 for(k=0;k<16;k++){
-printf("\%c ",tab[i]);
+printf("\%c ",tab1[i]);
 i++;
 }
 printf("/n");
